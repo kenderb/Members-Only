ba5079bf767581767674ab4b969740126ba3a57c
kevin Cotrina <kcotrinam92@gmail.com>
2020-11-25 17:33:30 -0500

    Add status and login, logout button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  helper_method :display_user
+  helper_method :display_button
+  helper_method :user_status
+  
+  def display_button (current_user)
+    return "<a href=#{destroy_user_session_path} data-method='delete' rel='nofollow'> Log out </a>".html_safe if current_user.present?
+              
+    "<a href=#{new_user_session_path}> Log in </a>".html_safe
+  end
+  
+  def display_user(username, post_author)
+    return post_author.username if username
+    'Not a member'
+  end
+  
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  
 end
